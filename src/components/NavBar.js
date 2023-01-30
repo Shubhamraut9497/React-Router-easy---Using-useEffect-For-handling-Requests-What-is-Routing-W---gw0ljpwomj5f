7386176fbd009380d5142cdd06d78952892645ee
Shubhamraut9497 <114891419+Shubhamraut9497@users.noreply.github.com>
2023-01-30 13:16:19 +0530

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React from "react";
 
-
-export const NavBar = () =>{
-    return(
-
-    )
-}
+import { Link } from "react-router-dom";
+export const NavBar = () => {
+  return (
+    <>
+      <nav>
+        <ul>
+          <Link to={"/"}>
+            <li>Index Page</li>
+          </Link>
+          <Link to={"/home"}>
+            <li>Home Page</li>
+          </Link>
+        </ul>
+      </nav>
+    </>
+  );
+};
